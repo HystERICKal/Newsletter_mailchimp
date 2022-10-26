1fb907c32e5c77d4d769896f820e2f2b59095bf8
HystERICKal <nyoroerick12@gmail.com>
2022-10-26 22:49:04 +0200

    Finished project...everything is working!

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 const port = 3000;
@@ -21,17 +22,65 @@ app.post("/", function(req, res){
     const uniqueListId = '0118ac38e9';
 
     //Get this from url in the address bar while signed in to your mail chimp account
+    //Also get it at the end of your API key
     const dataCenter = 'us21';
 
     console.log(firstName, lastName, email);
 
+    //Data to send to mailchimp with key value pairs that mailcimp will recognise
+    //The key and parameters are obtained from mailchimp api documentation
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: 'subscribed',
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    //Turn to flatpack JSON (basically a string int the JSON format)
+    //This is actually what we send to mailchimp
+    const jsonData = JSON.stringify(data);
+
+    const url = 'https://' + dataCenter + '.api.mailchimp.com/3.0/lists/' + uniqueListId;
+
+    //To know what options to use, go to https node module documentation
     const options = {
-        url: 'https://' + dataCenter + '.api.mailchimp.com/3.0/lists/' + uniqueListId,
+        method: 'POST',
+        //The first part of the auth key (erick1) can be anything
+        auth: 'erick1:21ad6f27db82e28982b73793f1fd00ab-us21'
     }
 
-    request(options, function(err, response, body){
+    //Send data to external resource
+    
+    //First, save the request to a constant called...request...lol
+    const request = https.request(url, options, function(response){
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
 
-    });
+        //Check if successful and display the appropriate html page
+        if (response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html");
+        }else{
+            res.sendFile(__dirname + "/failure.html");
+        }
+    }); 
+
+    //use the constant request created above to send the collected data to the mailchimp server
+    request.write(jsonData);
+
+    //Specifies the completion of a request
+    request.end();
+});
+
+//redirect to homepage if failure is encountered
+app.post("/failure", function(req, res){
+    res.redirect("/");
 });
 
 app.listen(port, function(){
